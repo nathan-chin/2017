ed71383b42ba1f1acd28dca483349907134dcc26
Nathan Chin <nathanchin@utexas.edu>
2017-07-20 03:15:08 -0500

    Most back-end complete!
    Shifting completely works now, only maybe need to fix how it scrolls up
    while shifting pages. Nav items now have a background color when
    selected. Still need to fix lagging of loading images, write website
    content, adapt for mobile, and adapt for other browsers

--- script.js
+++ script.js
@@ -5,6 +5,53 @@ $(document).ready(function(){
 	var pages = document.getElementsByClassName("page");
 	erasePages();
 	showPage(current);
+	$('#home-tab').css('background-color', '#393939');
+	$('#home-tab').css('box-shadow', 'inset 0 0 2px #000000');
+	
+	$('#home-tab').click(function(){
+		if(current != 0){
+			updatePage(0);
+			deselectTabs();
+			$(this).css('background-color', '#393939');
+			$(this).css('box-shadow', 'inset 0 0 2px #000000');
+		}
+	});
+	
+	$('#about-tab').click(function(){
+		if(current != 1){
+			updatePage(1);
+			deselectTabs();
+			$(this).css('background-color', '#393939');
+			$(this).css('box-shadow', 'inset 0 0 2px #000000');
+		}
+	});
+	
+	$('#hobbies-tab').click(function(){
+		if(current != 2){
+			updatePage(2);
+			deselectTabs();
+			$(this).css('background-color', '#393939');
+			$(this).css('box-shadow', 'inset 0 0 2px #000000');
+		}
+	});
+	
+	$('#projects-tab').click(function(){
+		if(current != 3){
+			updatePage(3);
+			deselectTabs();
+			$(this).css('background-color', '#393939');
+			$(this).css('box-shadow', 'inset 0 0 2px #000000');
+		}
+	});
+	
+	$('#contact-tab').click(function(){
+		if(current != 4){
+			updatePage(4);
+			deselectTabs();
+			$(this).css('background-color', '#393939');
+			$(this).css('box-shadow', 'inset 0 0 2px #000000');
+		}
+	});
 });
 
 var winHeight = $(window).height();
@@ -21,36 +68,39 @@ $(window).scroll(function(){
 });
 
 var updatePage = function(newTab){
-	erasePages();
 	if((current !== newTab) && (current < newTab)){
-		shiftLeft(current, newTab);
-	}
-	else if((current !== newTab) && (current > newTab)){
-		shiftRight(current, newTab);
+		shiftLeft(newTab);
+		//alert("Left");
 	}
 	else{
-		showPage(current);
-		alert("Wrong");
+		shiftRight(newTab);
+		//("Right");
 	}
+	$('html, body').animate({scrollTop: 0}, 700);
 };
 
 var erasePages = function(){
+	var special = current;
 	var pages = document.getElementsByClassName("page");
 	for(var i = 0; i < pages.length; i++){
-		pages[i].style.display = "none";
+		if(i !== special){
+			pages[i].style.display = "none";
+		}
 	}
 }
 
+/*
 var erasePage = function(tab){
 	var pages = document.getElementsByClassName("page");
 	pages[tab].style.display = "none";
-};
+};*/
 
 var showPage = function(tab){
 	var pages = document.getElementsByClassName("page");
 	pages[tab].style.display = "inline-block";
 };
 
+/*
 var getCurPage = function(){
 	var pages = document.getElementsByClassName("page");
 	for(var i = 0; i < pages.length; i++){
@@ -58,7 +108,7 @@ var getCurPage = function(){
 			return i;
 		}
 	}
-};
+};*/
 
 var getPage = function(num){
 	var home = $('#home');
@@ -80,40 +130,63 @@ var getPage = function(num){
 	}
 };
 
-var shiftLeft = function(curTab, newTab){
+var shiftLeft = function(newTab){
+
+	erasePages();
+	
 	var tempCur = getPage(current);
 	var tempNew = getPage(newTab);
 	
-	showPage(current);
-	showPage(newTab);
-	
 	var width = $(window).width();
+	tempCur.css('left', 0);
+	tempNew.css('left', 0);
+	showPage(current);
 	//var widthCur = tempCur.width() + parseInt(tempCur.css('margin-left'));
 	//var widthNew = tempNew.width() + parseInt(tempCur.css('margin-right')) + parseInt(tempNew.css('margin-left'));
-	$(tempCur).animate({left: -width}, {duration: 5000, queue: false});
-	$(tempNew).animate({left: -width}, {duration: 5000, queue: false});
+	$(tempCur).animate({left: -width}, {duration: 700, queue: false});
+	$(tempNew).animate({left: -width}, {duration: 700, queue: false});
+	//tempNew.css('left', -width);
+	showPage(newTab);
 	
-	$(tempNew).promise().done(function(){
+	/*$(tempNew).promise().done(function(){
 		erasePages();
 		current = newTab;
 		tempNew.css('left', '0');
 		showPage(newTab);
-	});
+	});*/
+	current = newTab;
+	
 };
 
-var shiftRight = function(curTab, newTab){
+var shiftRight = function(newTab){
+	erasePages();
+	
 	var tempCur = getPage(current);
 	var tempNew = getPage(newTab);
 	
 	var width = $(window).width();
-	$(tempNew).insertBefore(tempCur);
+	//$(tempNew).insertBefore(tempCur);
 	tempCur.css('left', -width);
+	tempNew.css('left', -width);
 	showPage(current);
 	//var widthCur = tempCur.width() + parseInt(tempCur.css('margin-right'));
 	//var widthNew = tempNew.width() + parseInt(tempCur.css('margin-left')) + parseInt(tempNew.css('margin-right'));
-	$(tempCur).animate({left: 0}, {duration: 5000, queue: false});
-	$(tempNew).animate({left: 0}, {duration: 5000, queue: false});
+	$(tempCur).animate({left: 0}, {duration: 700, queue: false});
+	$(tempNew).animate({left: 0}, {duration: 700, queue: false});
 	showPage(newTab);
 	current = newTab;
 };
+
+var deselectTabs = function(){
+	$('#home-tab').css('background-color', 'inherit');
+	$('#home-tab').css('box-shadow', 'none');
+	$('#about-tab').css('background-color', 'inherit');
+	$('#about-tab').css('box-shadow', 'none');
+	$('#hobbies-tab').css('background-color', 'inherit');
+	$('#hobbies-tab').css('box-shadow', 'none');
+	$('#projects-tab').css('background-color', 'inherit');
+	$('#projects-tab').css('box-shadow', 'none');
+	$('#contact-tab').css('background-color', 'inherit');
+	$('#contact-tab').css('box-shadow', 'none');
+}
 
