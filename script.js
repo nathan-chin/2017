cdfbe7ef29cbebfdbfe0fb75a5bdba38d55f8d3c
Nathan Chin <nathanchin@utexas.edu>
2017-07-26 23:14:00 -0500

    Completely restructuring website
    Need to fix resizing of "sticky" title headers, needs a LOT of fixing.
    Will fix shifting container to have projects, need to redesign page
    containers, delete/uncomment all unused code, attempted to add favicon
    but need to get that working too

--- script.js
+++ script.js
@@ -3,15 +3,35 @@ var current = 0;
 var old;
 var notFirst = 0;
 var ready = true;
+var winHeight, scrolls, shiftHeight, titleHeight;
+var aboutHeight, hobbiesHeight, projectsHeight, contactHeight;
+
 
 $(document).ready(function(){
+	aboutHeight = $('.about-title').offset().top;
+	hobbiesHeight = $('.hobbies-title').offset().top;
+	projectsHeight = $('.projects-title').offset().top;
+	contactHeight = $('.contact-title').offset().top;
 	var pages = document.getElementsByClassName("page");
 	erasePages();
 	showPage(current);
-	$('#home-tab').css('background-color', '#393939');
-	$('#home-tab').css('box-shadow', 'inset 0 0 2px #000000');
+	$('#about-tab').css('background-color', '#393939');
+	$('#about-tab').css('box-shadow', 'inset 0 0 2px #000000');
 	
-	$('#home-tab').click(function(e){
+	/*$('#home-tab').click(function(e){
+		e.preventDefault();
+		if(ready){
+			if(current !== 0){
+				ready = false;
+				updatePage(0);
+				deselectTabs();
+				$(this).css('background-color', '#393939');
+				$(this).css('box-shadow', 'inset 0 0 2px #000000');
+			}
+		}
+	});*/
+	
+	$('#about-tab').click(function(e){
 		e.preventDefault();
 		if(ready){
 			if(current !== 0){
@@ -24,7 +44,7 @@ $(document).ready(function(){
 		}
 	});
 	
-	$('#about-tab').click(function(e){
+	$('#hobbies-tab').click(function(e){
 		e.preventDefault();
 		if(ready){
 			if(current !== 1){
@@ -37,7 +57,7 @@ $(document).ready(function(){
 		}
 	});
 	
-	$('#hobbies-tab').click(function(e){
+	$('#projects-tab').click(function(e){
 		e.preventDefault();
 		if(ready){
 			if(current !== 2){
@@ -50,20 +70,7 @@ $(document).ready(function(){
 		}
 	});
 	
-	$('#projects-tab').click(function(e){
-		e.preventDefault();
-		if(ready){
-			if(current !== 3){
-				ready = false;
-				updatePage(3);
-				deselectTabs();
-				$(this).css('background-color', '#393939');
-				$(this).css('box-shadow', 'inset 0 0 2px #000000');
-			}
-		}
-	});
-	
-	$('#contact-tab').click(function(e){
+	/*$('#contact-tab').click(function(e){
 		e.preventDefault();
 		if(ready){
 			if(current !== 4){
@@ -74,20 +81,126 @@ $(document).ready(function(){
 				$(this).css('box-shadow', 'inset 0 0 2px #000000');
 			}
 		}
-	});
+	});*/
 });
 
-var winHeight = document.documentElement.clientHeight;
+window.addEventListener('resize', function(e){
+	$('#home-section').height($(window).height());
+	winHeight = document.documentElement.clientHeight;
+	scrolls = $(window).scrollTop();
+	shiftHeight = $('.shift-container').height();
+	erasePages();
+	var temp = getPage(current);
+	temp.css('left', 0);
+	showPage(current);
+});
+
 var onlyOnce = true;
 $(window).scroll(function(){
-	var scrolls = $(window).scrollTop();
-	if(scrolls > winHeight){
+	winHeight = document.documentElement.clientHeight;
+	scrolls = $(window).scrollTop();
+	shiftHeight = $('.shift-container').height();
+	titleHeight = $('.title').height();
+	if(scrolls < aboutHeight){
+		$('.about-title').css({
+			position: 'relative',
+			top: '0'
+		});
+	}
+	else if(scrolls >= aboutHeight && scrolls < hobbiesHeight){
+		$('.hobbies-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.projects-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.contact-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.about-title').css({
+			position: 'fixed',
+			top: '0',
+			left: '0'
+		});
+		$('#about-page').css('padding-top', titleHeight);
+	}
+	else if(scrolls >= hobbiesHeight && scrolls < projectsHeight){
+		$('.about-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.projects-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.contact-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.hobbies-title').css({
+			position: 'fixed',
+			top: '0',
+			left: '0'
+		});
+	}
+	else if(scrolls >= projectsHeight && scrolls < contactHeight){
+		$('.about-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.hobbies-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.contact-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.projects-title').css({
+			position: 'fixed',
+			top: '0',
+			left: '0'
+		});
+	}
+	else if(scrolls >= contactHeight){
+		$('.hobbies-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.projects-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.about-title').css({
+			position: 'relative',
+			top: '0'
+		});
+		$('.contact-title').css({
+			position: 'fixed',
+			top: '0',
+			left: '0'
+		});
+	}
+	/*if(scrolls > winHeight){
+		$('.shift-container').css('position', 'fixed');
+		$('.shift-container').css('width', '100%');
+		$('#no-intro-container').css('padding-top', shiftHeight);
+	}
+	else{
+		$('.shift-container').css('position', 'relative');
+		$('.shift-container').css('width', 'auto');
+		$('#no-intro-container').css('padding-top', '0');
+	}*/
+	/*if(scrolls > winHeight){
 		if(onlyOnce){
 			window.scrollTo(0,0);
 		}
 		onlyOnce = false;
 		$('#intro-container').css('display', 'none');		
-	}
+	}*/
 	/*$('.world-container').css('bottom', (scrolls * -0.2) - 300);
 	$('.intro-text').css('bottom', scrolls * 0.6);
 	$('.stars').css('bottom', scrolls * -0.2);
@@ -127,7 +240,7 @@ var erasePages = function(){
 			pages[i].style.display = "none";
 		}
 	}
-}
+};
 
 /*
 var erasePage = function(tab){
@@ -151,22 +264,18 @@ var getCurPage = function(){
 };*/
 
 var getPage = function(num){
-	var home = $('#home');
+	//var home = $('#home');
 	var about_me = $('#about-me');
 	var hobbies = $('#hobbies');
 	var projects = $('#projects');
-	var contact = $('#contact');
+	//var contact = $('#contact');
 	switch(num){
 		case 0:
-			return home;
-		case 1:
 			return about_me;
-		case 2:
+		case 1:
 			return hobbies;
-		case 3:
+		case 2:
 			return projects;
-		case 4:
-			return contact;
 	}
 };
 
@@ -178,7 +287,7 @@ var shiftLeft = function(newTab){
 	var tempNew = getPage(newTab);
 	
 	var width = $(window).width() + 4;
-	var height = $(window).outerHeight(true) + 1;
+	var height = $(window).outerHeight(true);
 	tempCur.css('left', 0);
 	tempNew.css('left', 0);
 	showPage(current);
@@ -186,14 +295,14 @@ var shiftLeft = function(newTab){
 	//var widthNew = tempNew.width() + parseInt(tempCur.css('margin-right')) + parseInt(tempNew.css('margin-left'));
 	var scrollDist = $(document).scrollTop();
 	if(scrollDist !== 0){
-		$('html, body').animate({scrollTop: 0}, 600, function(){
+		//$('html, body').animate({scrollTop: 0}, 600, function(){
 			$('.page').animate({left: -width}, 700, function(){
 				tempCur.find('.body-container').addClass('shrink');
 				ready = true;
 			});
 			//$(tempCur).animate({left: -width}, {duration: 700, queue: false});
 			//$(tempNew).animate({left: -width}, {duration: 700, queue: false});
-		});
+		//});
 	}
 	else{
 		$('.page').animate({left: -width}, 700, function(){
@@ -224,7 +333,7 @@ var shiftRight = function(newTab){
 	var tempNew = getPage(newTab);
 	
 	var width = $(window).width() + 4;
-	var height = $(window).outerHeight(true) + 1;
+	var height = $(window).outerHeight(true);
 	//$(tempNew).insertBefore(tempCur);
 	tempCur.css('left', -width);
 	tempNew.css('left', -width);
@@ -233,13 +342,13 @@ var shiftRight = function(newTab){
 	//var widthNew = tempNew.width() + parseInt(tempCur.css('margin-left')) + parseInt(tempNew.css('margin-right'));
 	var scrollDist = $(document).scrollTop();
 	if(scrollDist !== 0){
-		$('html, body').animate({scrollTop: 0}, 600, function(){
+		//$('html, body').animate({scrollTop: 0}, 600, function(){
 			$('.page').animate({left: 0}, 700, function(){
 				tempCur.find('.body-container').addClass('shrink');
 				ready = true;
 			});
 			//$(tempNew).animate({left: 0}, {duration: 700, queue: false});
-		});
+		//});
 	}
 	else{
 		$('.page').animate({left: 0}, 700, function(){
@@ -255,16 +364,16 @@ var shiftRight = function(newTab){
 };
 
 var deselectTabs = function(){
-	$('#home-tab').css('background-color', 'inherit');
-	$('#home-tab').css('box-shadow', 'none');
+	//$('#home-tab').css('background-color', 'inherit');
+	//$('#home-tab').css('box-shadow', 'none');
 	$('#about-tab').css('background-color', 'inherit');
 	$('#about-tab').css('box-shadow', 'none');
 	$('#hobbies-tab').css('background-color', 'inherit');
 	$('#hobbies-tab').css('box-shadow', 'none');
 	$('#projects-tab').css('background-color', 'inherit');
 	$('#projects-tab').css('box-shadow', 'none');
-	$('#contact-tab').css('background-color', 'inherit');
-	$('#contact-tab').css('box-shadow', 'none');
+	//$('#contact-tab').css('background-color', 'inherit');
+	//$('#contact-tab').css('box-shadow', 'none');
 };
 
 
